d0bba887aceedb687f22ef8374d7e7ae7a5c1e64
PalRishikesh <45008954+PalRishikesh@users.noreply.github.com>
2024-10-09 10:12:32 +0530

    Update Lambda-function.js response

--- Lambda-function.js
+++ Lambda-function.js
@@ -1,1 +1,16 @@
-console.log("hi");
+
+const jsonData =  [{"Dp_Record_Id": 2, 
+                  "DP_TYPE": "NSDL",
+                  "DP_ID": "40877589", 
+                  "CLIENT_ID": "1232", 
+                  "Default_flag": "Y"}]
+
+return {
+  "statusCode": 200,
+  "headers": {
+    "Access-Control-Allow-Origin": "*",
+    "Access-Control-Allow-Methods": "GET, POST, PUT, DELETE, OPTIONS",
+    "content-type":"application/json",
+  },
+  "body": jsonData
+}
